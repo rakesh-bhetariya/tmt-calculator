c2fe112472341dad48bc66313ddb8d9e905eec3d
Rakesh Bhetariya <rakeshbhetariya29@gmail.com>
2025-11-23 01:42:38 +0530

    remove courppted files

--- client/src/components/examples/CustomCalculator.tsx
+++
@@ -1,9 +0,0 @@
-import CustomCalculator from '../CustomCalculator';
-
-export default function CustomCalculatorExample() {
-  return (
-    <CustomCalculator 
-      onCalculate={(inputs) => console.log('Calculate:', inputs)} 
-    />
-  );
-}
--- client/src/components/examples/MixDesignTable.tsx
+++
@@ -1,6 +0,0 @@
-import MixDesignTable from '../MixDesignTable';
-import { standardMixDesigns } from '@shared/schema';
-
-export default function MixDesignTableExample() {
-  return <MixDesignTable designs={standardMixDesigns} />;
-}
